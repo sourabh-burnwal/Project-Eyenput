7cb42656740f48a9284b494297201dd631542bd1
Sourabh Kumar Burnwal <28016744+sourabh-burnwal@users.noreply.github.com>
2020-09-05 16:04:01 +0530

    Separate thresholding for both eyes, ear blink detection

--- eyenput_process.py
+++ eyenput_process.py
@@ -1,8 +1,9 @@
 import cv2
-import dlib
 from imutils import face_utils
 from math import hypot
 import numpy as np
+from scipy.spatial import distance
+
 
 def midpoint(p1, p2):
     return [(p1[0]+p2[0])//2, (p1[1]+p2[1])//2]
@@ -22,11 +23,7 @@ def get_bounds(region):
     max_y = np.max(region[:,1])
     return min_x, min_y, max_x, max_y
 
-def get_landmarks(frame, threshold):
-    #font = cv2.FONT_HERSHEY_SIMPLEX
-    p = r"C:\Users\soura\Desktop\project eyenput\shape_predictor_68_face_landmarks.dat"
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(p)
+def get_landmarks(frame, detector, predictor):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray_frame, 0)
     if len(faces) > 0:    
@@ -34,34 +31,43 @@ def get_landmarks(frame, threshold):
         face = face_utils.shape_to_np(landmarks)
         return face, gray_frame, landmarks
 
-def blink_counter(face):
-    blink = 0
+
+def eye_aspect_ratio(eyes_dots):
+    A = distance.euclidean(eyes_dots[1,:], eyes_dots[5,:])
+    B = distance.euclidean(eyes_dots[2,:], eyes_dots[4,:])
+    C = distance.euclidean(eyes_dots[0,:], eyes_dots[3,:])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def blink_counter(face, blink, counter):
+    ear_thres = 0.358
+    frames_per_blink = 5
+
     left_eye_dots = face[36:42]
     right_eye_dots = face[42:48]
-    left_eye_top, left_eye_bot, ratio_left = get_ratio(left_eye_dots)
-    right_eye_top, right_eye_bot, ratio_right = get_ratio(right_eye_dots)
-    #cv2.line(frame, (left_eye_top[0], left_eye_top[1]), (left_eye_bot[0], left_eye_bot[1]), color=(0,255,0), thickness=1)
-    #cv2.line(frame, (face[36,0], face[36,1]), (face[39,0], face[39,1]), color=(0,255,0), thickness=1)
-    #cv2.line(frame, (right_eye_top[0], right_eye_top[1]), (right_eye_bot[0], right_eye_bot[1]), color=(0,255,0), thickness=1)
-    #cv2.line(frame, (face[42,0], face[42,1]), (face[45,0], face[45,1]), color=(0,255,0), thickness=1)
-    aggr = (ratio_left+ratio_right) / 2
-    if aggr >= 18.755:
-        blink = blink+1
-    return blink
+    left_ear = eye_aspect_ratio(left_eye_dots)
+    right_ear = eye_aspect_ratio(right_eye_dots)
+    aggr_ear = left_ear + right_ear / 2
+    if aggr_ear < ear_thres:
+        counter += 1
+    if counter >= frames_per_blink:
+        blink += 1
+        counter = 0
+    return blink, counter
 
 def eye_prepare(eyes_mask, region, threshold):
     eye_min_x, eye_min_y, eye_max_x, eye_max_y = get_bounds(region)
     
     eye = eyes_mask[eye_min_y:eye_max_y, eye_min_x:eye_max_x]
-    #eye_resized = cv2.resize(eye, None, fx = 3, fy = 3)
-    _, eye_mod = cv2.threshold(eye, threshold, 255, cv2.THRESH_BINARY)
+    eye_resized = cv2.resize(eye, None, fx = 12, fy = 12)
+    _, eye_mod = cv2.threshold(eye_resized, threshold, 255, cv2.THRESH_BINARY)
     eye_mod = cv2.erode(eye_mod, None, iterations=2)
-    eye_mod = cv2.dilate(eye_mod, None, iterations=3)
+    eye_mod = cv2.dilate(eye_mod, None, iterations=4)
     eye_mod = cv2.medianBlur(eye_mod, 5)
     return eye_mod
 
 
-def eyes_preproc(landmarks, frame, gray_frame, threshold):
+def eyes_preproc(landmarks, frame, gray_frame, left_eye_thresh, right_eye_thresh):
     left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
                                 (landmarks.part(37).x, landmarks.part(37).y),
                                 (landmarks.part(38).x, landmarks.part(38).y),
@@ -84,7 +90,10 @@ def eyes_preproc(landmarks, frame, gray_frame, threshold):
     cv2.fillPoly(mask, [right_eye_region], 255)
 
     eyes_mask = cv2.bitwise_and(gray_frame, gray_frame, mask=mask)
-    left_eye_mod = eye_prepare(eyes_mask, left_eye_region, threshold)
-    right_eye_mod = eye_prepare(eyes_mask, right_eye_region, threshold)
+    left_eye_mod = eye_prepare(eyes_mask, left_eye_region, left_eye_thresh)
+    right_eye_mod = eye_prepare(eyes_mask, right_eye_region, right_eye_thresh)
     return left_eye_mod, right_eye_mod
     
+def detect_blob(eye, detector):
+    keypoints = detector.detect(eye)
+    return keypoints
